3b514fd01f86f333cfb6e199dfa239c436ee7664
kabhel <helene.kabbech@gmail.com>
2018-11-10 18:23:18 +0100

    Clean repository

--- src/test/test_dict.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-
-all = {}
-for i in range(1000000):
-  o = {}
-  o['i'] = i
-  o['l'] = []
-  all[i] = o
--- src/test/test_obj.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-class Obj(object):
-  def __init__(self, i):
-    self.i = i
-    self.l = []
-all = {}
-for i in range(1000000):
-  all[i] = Obj(i)
