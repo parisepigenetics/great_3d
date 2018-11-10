394e5eaca4605e8274bf9dca30debc06175e0e3b
kabhel <helene.kabbech@gmail.com>
2018-11-10 18:55:56 +0100

    Coments and pylint3

--- src/calculation.py
+++ src/calculation.py
@@ -1,18 +1,36 @@
 """
-.. module:: parsing
-   :synopsis: This module implements all the functions to parse either input
-                or additional necessary files.
+.. module:: calculation
+   :synopsis: This module implements the calculation part
 """
 
+# Third-party modules
 import numpy as np
 
-def calculate_transcription_map(coordinates, correlations, n, gene):
+def calculate_transcription_map(coordinates, correlations, nb_genes, gene):
+    """
+    Finds the n closest genes of a current gene and calcules the transcription map corresponding.
+
+    Args:
+        coordinates (Pandas Dataframe): X, Y, Z Coordinates of the genes
+        correlations (Pandas Dataframe): Correlations of the genes
+        nb_genes (int): Closest gene to take into account
+        gene (str): The studied gene
+
+    Returns:
+        float: The transcription map of the studied gene
+    """
+    # NaN values are set instead of the coordinate values of the current gene
+    # because the distance does not need to be calculated
     coordinates_copy = coordinates.copy()
     coordinates_copy.loc[gene] = np.NaN
-    distances =  np.sqrt((coordinates_copy['X'] - coordinates['X'][gene])**2 +\
-                         (coordinates_copy['Y'] - coordinates['Y'][gene])**2 +\
-                         (coordinates_copy['Z'] - coordinates['Z'][gene])**2)
+    # Calculation of the distance of the current gene with all genes
+    distances = np.sqrt((coordinates_copy['X'] - coordinates['X'][gene])**2 +\
+                        (coordinates_copy['Y'] - coordinates['Y'][gene])**2 +\
+                        (coordinates_copy['Z'] - coordinates['Z'][gene])**2)
+    # The distances are sorted and the n smallest are selected
+    # The corresponding genes represent the closest genes of the current gene
     distances = distances.sort_values(ascending=True, na_position='last')
-    close_genes = distances[:n].index
-    transcription_map = sum(abs(correlations[gene][close_genes]))
+    closest_genes = distances[:nb_genes].index
+    # The transcription map is calculated for the closest genes
+    transcription_map = sum(abs(correlations[gene][closest_genes]))
     return transcription_map
--- src/visualization.py
+++ src/visualization.py
@@ -1,28 +1,33 @@
 """
 .. module:: visualisation
-   :synopsis: This module implements all the functions to parse either input
-                or additional necessary files.
+   :synopsis: This module implements the 3D visualization part
 """
 
 # Third-party modules
-import matplotlib.cm as cmx
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.cm as cmx
 import matplotlib.pyplot as plt
 import matplotlib.colors as col
 
-def visualize_4d_genome(transcription_map, coordinates, colorsMap='YlOrRd'):
-    cs = transcription_map
-    cm = plt.get_cmap(colorsMap)
-    cNorm = col.Normalize(vmin=min(cs), vmax=max(cs))
-    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+def visualize_4d_genome(transcription_map, coordinates, colors_map='YlOrRd'):
+    """
+    Plot a 3D visualization of the genes colored by their transcription map.
+
+    Args:
+        transcription_map (list): Transcription map for the studied genes
+        coordinates (Pandas Dataframe): X, Y, Z Coordinates of the genes
+        colors_map (str): Chosen colors of the color bar. Default: YlOrRd (Yellow or Red)
+    """
+    col_map = plt.get_cmap(colors_map)
+    col_norm = col.Normalize(vmin=min(transcription_map), vmax=max(transcription_map))
+    scalar_map = cmx.ScalarMappable(norm=col_norm, cmap=col_map)
     fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    ax = Axes3D(fig)
-    ax.scatter(coordinates['X'], coordinates['Y'], coordinates['Z'],
-               c=scalarMap.to_rgba(cs), marker='o')
-    scalarMap.set_array(cs)
-    fig.colorbar(scalarMap)
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
+    axes = Axes3D(fig)
+    axes.scatter(coordinates['X'], coordinates['Y'], coordinates['Z'],\
+                 c=scalar_map.to_rgba(transcription_map), marker='o')
+    scalar_map.set_array(transcription_map)
+    fig.colorbar(scalar_map)
+    axes.set_xlabel('X Label')
+    axes.set_ylabel('Y Label')
+    axes.set_zlabel('Z Label')
     plt.show()
