fadd81cae175d225f5a9cefc49c9755abcfcdc49
Adam Bellaiche <adam.bellaiche@gmail.com>
2018-11-17 15:27:28 +0100

    pylint + distance

---
+++ src/map3d.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Program to print a 3D map"""
+
+import argparse
+import pandas
+from scipy.spatial.distance import squareform, pdist
+import file_manager
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description="3D transcription map, main file")
+    PARSER.add_argument('gene3Dpos', type=str,
+                        help='path to the file contains 3D positions of genes')
+    PARSER.add_argument('geneTranscriptionTable', type=str,
+                        help='path to the file contains expression of genes')
+
+    ARGS = PARSER.parse_args()
+
+
+    GENE_3D_POS = file_manager.check_file(ARGS.gene3Dpos)
+    GENE_EXPRESSION = file_manager.check_file(ARGS.geneTranscriptionTable)
+    DF_3DPOS = pandas.read_table(GENE_3D_POS, header=0)
+    DF_GENE_EXPR = pandas.read_table(GENE_EXPRESSION, header=0)
+    GENE_IN_3DPOS = list(DF_3DPOS.index)
+    GENE_IN_EXPR = list(DF_GENE_EXPR.index)
+    OVERLAP_GENE = set(GENE_IN_EXPR).intersection(GENE_IN_3DPOS)
+    DF_3DPOS = DF_3DPOS.loc[list(OVERLAP_GENE)]
+    DF_GENE_EXPR = DF_GENE_EXPR.loc[list(OVERLAP_GENE)]
+    DF_CORR = pandas.read_pickle("corr.pckl")
+    # DF_GENE_EXPR = DF_GENE_EXPR.transpose().corr(method="spearman")
+    DF_DIST = pandas.DataFrame(data=squareform(pdist(DF_3DPOS.drop('chr', 1),
+                                                     metric="euclidean")),
+                               index=DF_3DPOS.index,
+                               columns=DF_3DPOS.index)
