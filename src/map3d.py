c14e88c96deb26e3cd178976d78015dffe194c43
Adam Bellaiche <adam.bellaiche@gmail.com>
2019-01-03 18:37:20 +0100

    get the closer genes

--- src/map3d.py
+++ src/map3d.py
@@ -3,20 +3,24 @@
 
 import argparse
 import subprocess
+import time
 import pandas
+import numpy
 from scipy.spatial.distance import squareform, pdist
 import file_manager
 
+
 if __name__ == '__main__':
-    PARSER = argparse.ArgumentParser(description="3D transcription map, main file")
+    START = time.time()
+    PARSER = argparse.ArgumentParser(
+        description="3D transcription map, main file")
     PARSER.add_argument('gene3Dpos', type=str,
                         help='path to the file contains 3D positions of genes')
     PARSER.add_argument('geneTranscriptionTable', type=str,
                         help='path to the file contains expression of genes')
-
+    PARSER.add_argument('NCloserGenes', type=int,
+                        help='number of closer genes to take')
     ARGS = PARSER.parse_args()
-
-
     GENE_3D_POS = file_manager.check_file(ARGS.gene3Dpos)
     GENE_EXPRESSION = file_manager.check_file(ARGS.geneTranscriptionTable)
     DF_3DPOS = pandas.read_table(GENE_3D_POS, header=0)
@@ -24,13 +28,55 @@ if __name__ == '__main__':
     GENE_IN_3DPOS = list(DF_3DPOS.index)
     GENE_IN_EXPR = list(DF_GENE_EXPR.index)
     OVERLAP_GENE = set(GENE_IN_EXPR).intersection(GENE_IN_3DPOS)
+    # to have the same genes in the two matrix
     DF_3DPOS = DF_3DPOS.loc[list(OVERLAP_GENE)]
     DF_GENE_EXPR = DF_GENE_EXPR.loc[list(OVERLAP_GENE)]
     DF_GENE_EXPR.to_csv("../data/for_corr.csv", "\t")
     subprocess.check_call(['Rscript', 'correlation.R'], shell=False)
+    # called cor R function because it is faster than pandas.corr
     DF_CORR = pandas.read_table("../data/correlation.csv")
+    # DF_CORR = pandas.read_pickle("../data/corr.pckl")
     DF_DIST = pandas.DataFrame(data=squareform(pdist(DF_3DPOS.drop('chr', 1),
                                                      metric="euclidean")),
                                index=DF_3DPOS.index,
                                columns=DF_3DPOS.index)
-    
+    # caculate the distance matrix
+    DIST_LIST = [pandas.DataFrame]*len(DF_DIST)
+    DIST_DIC = {}
+    for i, val in enumerate(DF_DIST):
+        DIST_LIST[i]=DF_DIST.iloc[[i], :]
+        tmp_array=DIST_LIST[i].values.argsort() # sorted array 
+        tmp_index=[-1]*ARGS.NCloserGenes
+        count=ARGS.NCloserGenes-1
+        j=0
+        while(count>=0):
+            if(tmp_array[0][j] not in list(range(i-2,i+3))): # we don't take gene linearly closer
+                tmp_index[count]=tmp_array[0][j]
+                count=count - 1
+            j+=1
+        DIST_DIC[DIST_LIST[i].index[0]]=list(DIST_LIST[i].iloc[:,tmp_index].columns)
+
+    print(DIST_DIC)
+    print(time.time() - START)
+
+
+# Tu as un tableau de coordonnées x, y, z et un tableau d'expression des gènes
+# 1/ Il faut calculer la matrice de corrélation (tableau exp des gènes)
+# 2/ calcul des distances avec le tableau des coord
+# 3/ pour un gène donné, sélection des N gènes les plus proches puis tu sommes# la correlation des N gènes les plus proches # (utiliser la matrice de corrélation) cela te donne un chiffre X
+# 5/ Plot des coordonnées avec en couleur le chiffre X
+
+# DIST_LIST = [pd.DataFrame]*len(DF_DIST)
+# DIST_DIC = {}
+# for i, val in enumerate(DF_DIST):
+#     DIST_LIST[i]=DF_DIST.iloc[[i], :]
+#     tmp_array=DIST_LIST[i].values.argsort() # sorted array 
+#     tmp_index=[-1]*4
+#     count=4-1
+#     j=0
+#     while(count>=0):
+#         if(tmp_array[0][j] not in list(range(i-2,i+3))): # we don't take gene linearly closer
+#             tmp_index[count]=tmp_array[0][j]
+#             count=count - 1
+#         j+=1
+#     DIST_DIC[DIST_LIST[i].index[0]]=list(DIST_LIST[i].iloc[:,tmp_index].columns)
