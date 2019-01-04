fc8dedeaf388e86b9600cce031ab20fe6ecb600b
hocinebib <hocine.meraouna@yahoo.fr>
2019-01-04 10:46:06 +0100

    added opening exp and pos files using pandas

---
+++ src/openpadans.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+"""
+Genes position and expression files Parsing
+"""
+import argparse
+import pandas as pd
+
+def gene_position (position_file):
+    """
+    
+    """
+    return pd.read_csv(position_file, delim_whitespace = True)
+
+def gene_expression (expression_file):
+    """
+
+    """
+    return pd.read_csv(expression_file, delim_whitespace = True)
+
+
+if __name__ == "__main__":
+
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("pos_file", help="the file containing the x y z coordinates of the genes", type=str)
+    PARSER.add_argument("exp_file", help="the file containing the genes expression data", type=str)
+    ARGS = PARSER.parse_args()
+    POS_FILE = ARGS.pos_file
+    EXP_FILE = ARGS.exp_file
+    
+    GENE_POS = gene_position(POS_FILE)
+    print(GENE_POS)
+
+    GENE_EXP = gene_expression(EXP_FILE)
+    print(GENE_EXP)
