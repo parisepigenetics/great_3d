65b74fd23f1d02ff6b8c21fc2f25ebe07f21815f
Adam Bellaiche <adam.bellaiche@gmail.com>
2019-01-04 16:52:20 +0100

    plot ok

--- src/map3d.py
+++ src/map3d.py
@@ -5,7 +5,9 @@ import argparse
 import subprocess
 import time
 import pandas
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.distance import squareform, pdist
 import file_manager
 
@@ -27,7 +29,7 @@ if __name__ == '__main__':
     DF_GENE_EXPR = pandas.read_table(GENE_EXPRESSION, header=0)
     GENE_IN_3DPOS = list(DF_3DPOS.index)
     GENE_IN_EXPR = list(DF_GENE_EXPR.index)
-    OVERLAP_GENE = set(GENE_IN_EXPR).intersection(GENE_IN_3DPOS)
+    OVERLAP_GENE = set(GENE_IN_EXPR).intersection(GENE_IN_3DPOS) # mettre une erreur si rien ne s'overlap
     # to have the same genes in the two matrix
     DF_3DPOS = DF_3DPOS.loc[list(OVERLAP_GENE)]
     DF_GENE_EXPR = DF_GENE_EXPR.loc[list(OVERLAP_GENE)]
@@ -40,6 +42,7 @@ if __name__ == '__main__':
                                                      metric="euclidean")),
                                index=DF_3DPOS.index,
                                columns=DF_3DPOS.index)
+    print("{} secondes to create matrix".format(time.time() - START))
     # caculate the distance matrix
     DIST_LIST = [pandas.DataFrame]*len(DF_DIST)
     # dictionary with gene name for key and list of N closer genes for values
@@ -60,12 +63,29 @@ if __name__ == '__main__':
         DIST_DIC[DIST_LIST[i].index[0]]=list(DIST_LIST[i].iloc[:,tmp_index].columns)
         sum_corr=0
         for closer_gene in DIST_DIC[DIST_LIST[i].index[0]]:
-            sum_corr = sum_corr + DF_CORR [DIST_LIST[i].index[0]][closer_gene]
+            sum_corr = sum_corr + abs(DF_CORR[DIST_LIST[i].index[0]][closer_gene])
+            print(abs(DF_CORR[DIST_LIST[i].index[0]][closer_gene]))
         coord=DF_3DPOS.loc[DIST_LIST[i].index[0]].values[1:]
         PLOT_DIC[DIST_LIST[i].index[0]]=[coord[0], coord[1], coord[2], sum_corr]
-
-    print(PLOT_DIC)
-    print(time.time() - START)
+    print("{} secondes to create PLOT_DIC".format(time.time() - START))
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    xs = [-1]*len(PLOT_DIC)
+    ys = [-1]*len(PLOT_DIC)
+    zs = [-1]*len(PLOT_DIC)
+    color = [-1]*len(PLOT_DIC)
+    for i, gene in enumerate(PLOT_DIC):
+        xs[i]=PLOT_DIC[gene][0]
+        ys[i]=PLOT_DIC[gene][1]
+        zs[i]=PLOT_DIC[gene][2]
+        color[i]=PLOT_DIC[gene][3]
+    p = ax.scatter(xs, ys, zs, c=color, cmap="spring", marker="o")
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    fig.colorbar(p)
+    print("{} secondes to Plot".format(time.time() - START))
+    plt.show()
 
 
 # Tu as un tableau de coordonnées x, y, z et un tableau d'expression des gènes
@@ -73,18 +93,3 @@ if __name__ == '__main__':
 # 2/ calcul des distances avec le tableau des coord
 # 3/ pour un gène donné, sélection des N gènes les plus proches puis tu sommes# la correlation des N gènes les plus proches # (utiliser la matrice de corrélation) cela te donne un chiffre X
 # 5/ Plot des coordonnées avec en couleur le chiffre X
-
-# DIST_LIST = [pd.DataFrame]*len(DF_DIST)
-# DIST_DIC = {}
-# for i, val in enumerate(DF_DIST):
-#     DIST_LIST[i]=DF_DIST.iloc[[i], :]
-#     tmp_array=DIST_LIST[i].values.argsort() # sorted array 
-#     tmp_index=[-1]*4
-#     count=4-1
-#     j=0
-#     while(count>=0):
-#         if(tmp_array[0][j] not in list(range(i-2,i+3))): # we don't take gene linearly closer
-#             tmp_index[count]=tmp_array[0][j]
-#             count=count - 1
-#         j+=1
-#     DIST_DIC[DIST_LIST[i].index[0]]=list(DIST_LIST[i].iloc[:,tmp_index].columns)
