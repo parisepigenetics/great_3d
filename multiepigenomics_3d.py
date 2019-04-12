20c96cb69662c78abce85caf65f8935e81ea7728
Costas Bouyioukos <cbouyio@gmail.com>
2019-04-12 12:29:27 +0200

    First working prototype of the distance sorting. Needs to be parallelised. TODO for Miara.

--- multiepigenomics_3d.py
+++ multiepigenomics_3d.py
@@ -1,33 +1,38 @@
-#!/usr/bin/python
 # -*- coding: utf-8  -*-
 
-import pandas as pd
+"""Main multi epigenetics viewer moddule.
+"""
+
+import math
 import numpy as np
 import scipy
-from scipy.spatial import distance_matrix
 from multiprocessing import Pool
-import math
+import pandas as pd
+from scipy.spatial import distance_matrix
 
-def distance_matrice(data):
-    '''
-    Function that reads a gene position or a gene expression file
-    and return the distance Matrice into a DataFrame
-    '''
-    dt = pd.read_csv(data, sep = '\t')
-    ndarray = scipy.spatial.distance.pdist(dt)
+def matrix_distance(data):
+    """Gets a position filename, return the distance matrix as a Pandas DataFrame.
+    """
+    df = pd.read_csv(data, sep='\t')
+    geneNames = df.index
+    ndarray = scipy.spatial.distance.pdist(df)
     matrix_uni = scipy.spatial.distance.squareform(ndarray)
-    matrice_dist = pd.DataFrame(matrix_uni)
-    for i in range(len(matrice_dist)) :
-        matrice_dist= matrice_dist.rename(columns= {i:dt.index[i]});
-        matrice_dist= matrice_dist.rename(index= {i:dt.index[i]})
-    return matrice_dist
+    matrix_dist = pd.DataFrame(matrix_uni)
+    matrix_dist.columns = geneNames
+    matrix_dist.index = geneNames
+    #for i in range(len(matrix_dist)):  # No need to do this copmplicated loop.
+    #    matrix_dist = matrix_dist.rename(columns={i:df.index[i]})
+    #    matrix_dist = matrix_dist.rename(index={i:df.index[i]})
+    return matrix_dist
 
-def dico_matrice(matrice, nb_genes):
+def dico_matrix(matrix):
+    '''Get a distance matrix (Pandas DataFrame) and return a dictionary of sorted genes with each gene as a key.
     '''
-    Function that return the N closest genes
-    '''
-    dico = {}
-    for i in range(len(matrice)):
-        #dico[dt.index[i]] =  matrice[dt.index[i]].sort_values()[1:10].index
-        dico[matrice.index[i]] =   matrice[matrice.index[i]].sort_values()[1:nb_genes+1]
-    return dico
+    sortingDict = {}
+    # TODO put multiprocessing and parallelise this for loop ONLY (for the moment).
+    for i in range(len(matrix)):  # Loop over the data frame row names (i.e. gene names).
+        sortingDict[matrix.index[i]] = matrix[matrix.index[i]].sort_values()
+    return sortingDict
+
+# If you want put it here.
+#print('le nombre de processeur utilisé est de : ', multiprocessing.cpu_count())
