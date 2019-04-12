2ba56f180357a1e0bd0530b711340b9ff12cd0f8
Costas Bouyioukos <cbouyio@gmail.com>
2019-04-12 17:08:43 +0200

    Complete the aralelisation of the distance sorting. Add several TODOs for Miara.

--- multiepigenomics_3d.py
+++ multiepigenomics_3d.py
@@ -4,19 +4,22 @@
 """
 
 import math
+import time
 import numpy as np
 import scipy
-from multiprocessing import Pool
+from multiprocessing import cpu_count, Pool
 import pandas as pd
 from scipy.spatial import distance_matrix
 
+
 def matrix_distance(data):
+    # TODO parallelise the calculation of the distance matrix.
     """Gets a position filename, return the distance matrix as a Pandas DataFrame.
     """
     df = pd.read_csv(data, sep='\t')
     geneNames = df.index
-    ndarray = scipy.spatial.distance.pdist(df)
-    matrix_uni = scipy.spatial.distance.squareform(ndarray)
+    ndarray = distance.pdist(df)
+    matrix_uni = distance.squareform(ndarray)
     matrix_dist = pd.DataFrame(matrix_uni)
     matrix_dist.columns = geneNames
     matrix_dist.index = geneNames
@@ -30,9 +33,25 @@ def dico_matrix(matrix):
         sortingDict[gene_name] = matrix[gene_name].sort_values()
     return sortingDict
 
-def parallelization_dico_matrix(matrix) :
+def parallelise_sorting_matrix(df) :
     '''Running the dico_matrix function using Multiprocessing
     '''
-    with Pool(5) as p :
-    	sortingDict_multi = p.map(dico_matrix,[matrix])
-    return sortingDict_multi
+    cpus = cpu_count() - 1
+    with Pool(cpus) as pool:
+        # Split the data frame.
+        df_split = np.array_split(df, cpus)
+        sorted_list_df = pool.map(dico_matrix, df_split)
+        pool.close
+    return sorted_list_df
+
+
+## Nice wrapper to time functions. Works as a decorator.
+# Taken from https://stackoverflow.com/questions/5478351/python-time-measure-function
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
+        return ret
+    return wrap
