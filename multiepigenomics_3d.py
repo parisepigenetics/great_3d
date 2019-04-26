Final commit of the caclulation of the distance sorted dictionary. Clean up a BAD bug with the calculation of the sorting of the dictionary values, drop out the parallelisation of the sorting as it makes the machine unstable and it does not run any faster than the default method. Code NOT working.

multiepigenomics_3d.py
@@ -11,50 +11,6 @@ from multiprocessing import cpu_count, Pool
 import pandas as pd
 from scipy.spatial import distance_matrix
 
-
-def matrix_distance(data):
-    # TODO parallelise the calculation of the distance matrix.
-    """Gets a position filename, return the distance matrix as a Pandas DataFrame.
-    """
-    df = pd.read_csv(data, sep='\t')
-    geneNames = df.index
-    ndarray = scipy.spatial.distance.pdist(df)
-    matrix_uni = scipy.spatial.distance.squareform(ndarray)
-    matrix_dist = pd.DataFrame(matrix_uni)
-    matrix_dist.columns = geneNames
-    matrix_dist.index = geneNames
-    return matrix_dist
-
-def dico_matrix(matrix):
-    '''Get a distance matrix (Pandas DataFrame) and return a dictionary of sorted genes with each gene as a key.
-    '''
-    sortingDict = {}
-    for gene_name in matrix.index:
-        sortingDict[gene_name] = matrix[gene_name].sort_values()
-    return sortingDict
-
-def parallelise_sorting_matrix(df) :
-    '''Running the dico_matrix function using Multiprocessing
-    '''
-    #cpus = cpu_count() - 1
-    cpus = 1
-    with Pool(cpus) as pool:
-        # Split the data frame
-        df_split = np.array_split(df, cpus)
-        sorted_list_df = pool.map(dico_matrix, df_split)
-        pool.close()
-        pool.join()
-    return sorted_list_df
-
-def closest_gene_name(dico_closest_gene):
-    '''Get the Dictionnary of sorted genes and return only the name of the N closest gene with each gene as a key
-    '''
-    dico_name_closest_gene = {}
-    for gene_name in dico_closest_gene :
-        closest_genes = list(pd.DataFrame(dico_closest_gene[gene_name][1:]).index)
-        dico_name_closest_gene[gene_name] = closest_genes
-    return dico_name_closest_gene
-
 ## Nice wrapper to time functions. Works as a decorator.
 # Taken from https://stackoverflow.com/questions/5478351/python-time-measure-function
 def timing(f):
@@ -66,19 +22,69 @@ def timing(f):
         return ret
     return wrap
 
-def correlation_matrix(gene_expression_file):
+
+def calculate_distance(position_file):
+    # TODO parallelise the calculation of the distance matrix.
+    """Get a position file, return the distance matrix as a Pandas DataFrame.
     """
-    Gets the gene Expression filename, return the Correlation matrix as a Pandas DataFrame.
-    """
-    gene_expression_df = pd.read_csv(gene_expression_file , sep ='\t').transpose()
-    return gene_expression_df.corr(method = 'spearman') #or Pearson
+    df = pd.read_csv(position_file, sep='\t')
+    geneNames = df.index
+    ndarray = scipy.spatial.distance.pdist(df)
+    matrix_uni = scipy.spatial.distance.squareform(ndarray)
+    matrix_dist = pd.DataFrame(matrix_uni)
+    matrix_dist.columns = geneNames
+    matrix_dist.index = geneNames
+    return matrix_dist
+
+
+def sorting_distances(dist_df):
+    '''Get a distance matrix (Pandas DataFrame) and return a dictionary of sorted genes with each gene as a key.
+    '''
+    sortedDict = {}
+    for gene_name in dist_df.index:
+        sortedDict[gene_name] = dist_df.loc[gene_name,].sort_values()
+    return sortedDict
+
 
 def sum_Correlation(distance_dico , correlation_matrix):
-    """
-    Gets the Dictionnary of the closest_gene and the correlation Matrix, return a Dictionnary of the sum of correlation for each gene
+    """Gets the Dictionnary of the closest_gene and the correlation Matrix, return a Dictionnary of the sum of correlation for each gene
     """
     dico_sum_Correlation = {}
     for gene , closest_gene in distance_dico.items():
         sum_correlation = abs(correlation_matrix.loc[gene,closest_gene].sum())
         dico_sum_Correlation[gene] = sum_correlation
     return dico_sum_Correlation
+
+
+
+#================ OBSOLETE functions ======================
+
+def sorting_dists_parallel(dist_df) :
+    '''Get a distance matrix (pandas data frame), return a dictionary of genes and their sorted neighbours.
+    '''
+    cpus = cpu_count() - 1
+    sorted_dict = {}
+    with Pool(cpus) as pool:
+        # Split the data frame
+        df_split = np.array_split(dist_df, cpus)
+        sorted_dfs = pool.map(sorting_distances, df_split)
+        pool.close()
+        pool.join()
+    # Put together all the results.
+    for d in sorted_dfs: sorted_dict.update(d)
+    return sorted_dict
+
+def closest_gene_name(dico_closest_gene):
+    '''Get the dictionnary of sorted genes and return only the names of the N closest gene with each gene as a key
+    '''
+    dico_name_closest_gene = {}
+    for gene_name in dico_closest_gene :
+        closest_genes = list(pd.DataFrame(dico_closest_gene[gene_name][1:]).index)
+        dico_name_closest_gene[gene_name] = closest_genes
+    return dico_name_closest_gene
+
+def correlation_matrix(gene_expression_file):
+    """Gets the gene Expression filename, return the Correlation matrix as a Pandas DataFrame.
+    """
+    gene_expression_df = pd.read_csv(gene_expression_file , sep ='\t').transpose()
+    return gene_expression_df.corr(method = 'spearman') #or Pearson