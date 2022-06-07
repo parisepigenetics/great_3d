1a7468cf94cec81a95c37d0450a95803f43d858c
Costas Bouyioukos <costas.bouyioukos@u-paris.fr>
2022-06-07 23:33:11 +0300

    Initial commit of the module dealing with the direct integration of genomic data.

---
+++ great_3d/genome_3D_integration.py
@@ -0,0 +1,2 @@
+"""Module to deal with the translation of genomic coordinates originating from a standard annotation file (i.e. GTF) into Hi-C fragments/bins cordinates for integration to the GREAT framework analysis.
+"""
