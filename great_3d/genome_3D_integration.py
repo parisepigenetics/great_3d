7491ca81e59c80a6e4e27ce33fa8a58e469e54ff
Costas Bouyioukos <cbouyio@gmail.com>
2024-01-25 14:24:00 +0200

    Remove some extraneous seek() commands and remove a hardcoded varianble. More to optimise in a later pass.

--- great_3d/genome_3D_integration.py
+++ great_3d/genome_3D_integration.py
@@ -7,7 +7,7 @@ integration to the GREAT framework analysis.
 import pandas as pd
 
 
-def compute_gene_coordinates(pos_file_Genes, pos_file_Genome3D):
+def compute_gene_coordinates(pos_file_Genes, pos_file_Genome3D, proximity=5000):
     """Get two tab files, one with the genenames andtheir respective start sites and one with the 3D genome coordinates (as midpoints of fragments of fixed length i.e. resoluton).
 
     Return a dataFrame with the 3D coordinates of genes.
@@ -15,17 +15,15 @@ def compute_gene_coordinates(pos_file_Genes, pos_file_Genome3D):
     # TODO in the first run the closest known midpoint will be considered, however n later versions amore proper geometric clculation of the 3D position of a gene must be considered.
     dfGenome = pd.read_table(pos_file_Genome3D)
     dfGenes = pd.read_table(pos_file_Genes)
-    pos_file_Genome3D.seek(0, 0)
-    pos_file_Genes.seek(0, 0)
     # First simple approach, the gene is assigned to its closest bin median point.
     # Check chromosome consistancy
     chromsGenome = list(set(dfGenome.loc[:, "chr"].tolist()))
     chromsGenes = list(set(dfGenes.loc[:, "chr"].tolist()))
     if chromsGenes != chromsGenome:
-        raise Exception("Chromosome consistence between genes and the genome is not met. Check the input files.")
+        raise Exception("Chromosome consistency between genes and the genome is not met. Check the input files.")
     # Prepare the data frame lists
     names = []
-    chr = []
+    chrs = []
     Xs = []
     Ys = []
     Zs = []
@@ -35,10 +33,10 @@ def compute_gene_coordinates(pos_file_Genes, pos_file_Genome3D):
         dfChromGenome = dfGenome[dfGenome["chr"] == ch]
         for row1 in dfChromGene.itertuples():
             for row2 in dfChromGenome.itertuples():
-                if abs(row1.start - row2.midpoint) < 5000:  # FIXME THIS MUST NOT BE HARDCODED!!!
+                if abs(row1.start - row2.midpoint) < proximity:
                     break
             names.append(row1.Index)
-            chr.append(ch)
+            chrs.append(ch)
             Xs.append(row2.X)
             Ys.append(row2.Y)
             Zs.append(row2.Z)
