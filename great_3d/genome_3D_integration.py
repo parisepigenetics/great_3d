63ebd5ca08cee6a1ba99d3199463d8b2a357fb9c
Costas Bouyioukos <cbouyio@gmail.com>
2024-01-25 23:27:50 +0200

    Proper implementation of gene posiytions file based on geometric reconstruction of start sites.

--- great_3d/genome_3D_integration.py
+++ great_3d/genome_3D_integration.py
@@ -4,26 +4,32 @@ Originating from a standard annotation file (i.e. GTF) into Hi-C fragments/bins 
 integration to the GREAT framework analysis.
 """
 
+import math
 import pandas as pd
 
 
-def compute_gene_coordinates(pos_file_Genes, pos_file_Genome3D, proximity=5000):
-    """Get two tab files, one with the genenames and their respective start sites and one with the 3D genome coordinates (as midpoints of fragments of fixed length i.e. resoluton).
-
-    Return a dataFrame with the 3D coordinates of genes.
+def compute_gene_coordinates(start_sites_Genes, pos_file_Genome3D, resolution):
+    """Get two tab files, one with the gene names and their respective start sites and one with the 3D genome coordinates (as midpoints of fragments of fixed length i.e. resolution).
+    
+    - Args:
+    - `starts_sites_Genes`: The file path to the gene names and start sites tab file.
+    - `pos_file_Genome3D`: The file path to the 3D genome coordinates tab file.
+    - `resolution`: The fixed length of the fragments used for 3D genome coordinates.
+    - Return:
+    - A DataFrame with the 3D coordinates of genes.
     """
-    # TODO in the first run the closest known midpoint will be considered, however on later versions a more proper geometric calculation of the 3D position of a gene must be considered.
     dfGenome = pd.read_table(pos_file_Genome3D)
-    dfGenes = pd.read_table(pos_file_Genes)
+    dfGenes = pd.read_table(start_sites_Genes)
     # First simple approach, the gene is assigned to its closest bin median point.
     # Check chromosome consistancy
     chromsGenome = list(set(dfGenome.loc[:, "chr"].tolist()))
     chromsGenes = list(set(dfGenes.loc[:, "chr"].tolist()))
     if chromsGenes != chromsGenome:
-        raise Exception("Chromosome consistency between genes and the genome is not met. Check the input files.")
+        raise ValueError("Chromosome inconsistency between genes file and genome file. Check the input files.")
     # Prepare the data frame lists
     names = []
     chrs = []
+    starts = []
     Xs = []
     Ys = []
     Zs = []
@@ -31,19 +37,28 @@ def compute_gene_coordinates(pos_file_Genes, pos_file_Genome3D, proximity=5000):
     for ch in chromsGenes:
         dfChromGene = dfGenes[dfGenes["chr"] == ch]
         dfChromGenome = dfGenome[dfGenome["chr"] == ch]
-        for row1 in dfChromGene.itertuples():
-            for row2 in dfChromGenome.itertuples():
-                if abs(row1.start - row2.midpoint) < proximity:
+        for gene in dfChromGene.itertuples():
+            for point in dfChromGenome.itertuples():
+                if abs(gene.start - point.midpoint) < resolution:
+                    pointBefor = point
+                    pointAfter = dfChromGenome[dfChromGenome["midpoint"] == point.midpoint + resolution]
                     break
-            names.append(row1.Index)
+            xb, yb, zb = pointBefor.X, pointBefor.Y, pointBefor.Z
+            xa, ya, za = pointAfter.iloc[0].X, pointAfter.iloc[0].Y, pointAfter.iloc[0].Z
+            distance = math.sqrt((xa - xb)**2 + (ya - yb)**2 + (za - zb)**2)
+            a_bNorm = [(xa - xb) / distance, (ya - yb) / distance, (za - zb) / distance]
+            geneCoef = ((gene.start - point.midpoint) / resolution) * distance
+            geneExtr = [e * geneCoef for e in a_bNorm]
+            names.append(gene.Index)
             chrs.append(ch)
-            Xs.append(row2.X)
-            Ys.append(row2.Y)
-            Zs.append(row2.Z)
+            starts.append(gene.start)
+            Xs.append(point.X + geneExtr[0])
+            Ys.append(point.Y + geneExtr[1])
+            Zs.append(point.Z + geneExtr[2])
     # Generate the new data frame and reurn it
-    df = pd.DataFrame({"chr": chr, "X": Xs, "Y": Ys, "Z": Zs})
+    df = pd.DataFrame({"chr": chr, "start": starts, "X": Xs, "Y": Ys, "Z": Zs})
     df.index = names
     # Reset the files as they needed for later computations
     pos_file_Genome3D.seek(0)
-    pos_file_Genes.seek(0)
+    start_sites_Genes.seek(0)
     return df
